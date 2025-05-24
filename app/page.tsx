e3ee56faef91b0d1d93a72806cab1c4f0255593c
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-05-24 20:54:32 +0800

    lab-exercise

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+import styles from '@/app/ui/home.module.css';
+ 
+export default function Page() {
+  return (
+    // ...
+    <p
+      className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+    > <AcmeLogo />
+      <div className={styles.shape} />
+      <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}> 
+      </p>
+      <strong>Welcome to Acme.</strong> This is the example for the{' '}
+      <a href="https://nextjs.org/learn/" className="text-blue-500">
+        Next.js Learn Course
+      </a>
+      , brought to you by Vercel.
+    </p>
+    // ...
+  );
+}
